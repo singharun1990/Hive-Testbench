225bfd36911d1fc0234f76ad6357e64d02b1602b
singharun1990 <arusi@microsoft.com>
2018-06-22 00:55:15 +0530

    Update config.sh

--- workload/tpch/config.sh
+++ workload/tpch/config.sh
@@ -7,12 +7,12 @@ then
 fi
 WORKLOAD=tpch
 CONNECTION_STRING=jdbc:hive2://localhost:10001
-WAREHOUSE_DIR=/hive/warehouse
+WAREHOUSE_DIR=abfs://tpchcontainer1@bn3blobfs.dfs.core.windows.net/hive/warehouse
 RAWDATA_DATABASE=tpch_text_${SCALE}
 QUERY_DATABASE=tpch_partitioned_orc_${SCALE}
 TABLES="part partsupp supplier customer orders lineitem nation region"
 DEBUG_ON=true
-RAWDATA_DIR=/tmp/tpch-generate
+RAWDATA_DIR=abfs://tpchcontainer1@bn3blobfs.dfs.core.windows.net/tmp/tpch-generate
 RUN_ANALYZE=true
 CURRENT_DIRECTORY=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 LOADTIMES_DIR=${CURRENT_DIRECTORY}/../../output/$WORKLOAD/loadtimes/
